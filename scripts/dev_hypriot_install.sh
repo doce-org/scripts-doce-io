537216509ae068a2cd9dddab2d7ba1c3b7424508
Shad <shadowrr2@gmail.com>
2017-06-18 11:26:32 +0200

    fix on cd.. path

--- scripts/dev_hypriot_install.sh
+++ scripts/dev_hypriot_install.sh
@@ -3,9 +3,6 @@
 # install apt-get on hypriot
 curl -s https://packagecloud.io/install/repositories/Hypriot/Schatzkiste/script.deb.sh | sudo bash
 
-# run the postgres docker container
-docker run --name doce-postgres -e POSTGRES_PASSWORD=doce -d tobi312/rpi-postgresql
-
 # prepare the project folder
 mkdir app
 cd app
@@ -13,11 +10,13 @@ cd app
 # setup the api
 git clone https://github.com/doce-org/api-doce-io.git api.doce.io
 cd api.doce.io/scripts
+chmod +x *
 sh rpi_dev_docker.sh
-cd
+cd ../..
 
 # setup the web app
 git clone https://github.com/doce-org/web-doce-io.git web.doce.io
-cd web.doce.io/dist
-npm run postinstall
+cd web.doce.io/scripts
+chmox +x *
+sh rpi_dev_docker.sh
 
