9a6f64f70defd3ad27c149ddbba8814f8848f6ff
Shad <shadowrr2@gmail.com>
2017-06-25 15:41:21 +0200

    echo logs fix

--- scripts/dev_hypriot_install.sh
+++ scripts/dev_hypriot_install.sh
@@ -3,10 +3,10 @@
 setup_doce_api_server() {
 
     # setup the api
-    echo -n 'Downloading the api server...'
+    echo 'Downloading the api server...'
     git clone https://github.com/doce-org/api-doce-io.git api.doce.io
 
-    echo -n 'Launching api server docker container...'
+    echo 'Launching api server docker container...'
     cd api.doce.io/scripts
     chmod +x *
     sh rpi_dev_docker.sh
@@ -17,10 +17,10 @@ setup_doce_api_server() {
 setup_doce_web_app() {
 
     # setup the web app
-    echo -n 'Downloading the web application...'
+    echo 'Downloading the web application...'
     git clone https://github.com/doce-org/web-doce-io.git web.doce.io
 
-    echo -n 'Launching web app docker container...'
+    echo 'Launching web app docker container...'
     cd web.doce.io/scripts
     chmod +x *
     sh rpi_dev_docker.sh
@@ -30,7 +30,7 @@ setup_doce_web_app() {
 main() {
 
     # prepare the project folder
-    echo -n 'Preparing installation folder...'
+    echo 'Preparing installation folder...'
     if [ -d "app" ]; then
         sudo rm -rf app
     fi
@@ -45,8 +45,8 @@ main() {
 
     cd
 
-    echo -n 'Installation is finished. You may wait for a few minutes, until all docker are up and running...'
-    echo -n 'You will then be able to access D.O.C.E from a web browser at this address: http:/black-pearl:8080/ (local only)'
+    echo 'Installation is finished. You may wait for a few minutes, until all docker are up and running...'
+    echo 'You will then be able to access D.O.C.E from a web browser at this address: http:/black-pearl:8080/ (local only)'
 
 }
 
